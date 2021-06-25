d93514d27613a6c32e71092a4eb0263f187c99b2
Obaro Winner <85342925+winnerneo@users.noreply.github.com>
2021-06-25 06:58:31 -0400

    script

---
+++ java script/script.js
@@ -0,0 +1,62 @@
+function myfunction(){
+    document.getElementById('demo').innerHTML ="The Paragraph has changed"
+}
+document.write("emeka");
+//window.alert("frontend")
+console.log(900);
+ 
+var x = 5;
+let y = 6; 
+
+var z = x + y;
+
+console.log(z) 
+
+var person ='Winner' ; //string data type
+var age = 15 ; //you dont need any qoute
+var rightWrong = true; //its either true or false 
+var numChildren = null;
+var definition = undefined;// when you declare a varible without add any value to it gives undifined 
+
+//console.log
+console.log(person)
+console.log(age)
+console.log(rightWrong)
+console.log(numChildren)
+
+//innerhtml
+
+document.getElementById('name').innerHTML = person
+document.getElementById('number').innerHTML = age
+document.getElementById('question').innerHTML = true
+document.getElementById('ask').innerHTML = null
+
+
+
+//addition operators
+var number1 = 5;
+var number2 = 20;
+var sum = number1 + number2;
+
+console.log(sum)
+
+//multiplication operators
+
+var a = 5;
+var b = 6;
+
+var multiple = a * b;
+
+console.log(multiple)
+
+//increament
+var s = 100
+s++;
+console.log('increament operator', s);
+
+var num = 16
+undefined
+var num1 = '16'
+undefined
+num = num1
+"16"
